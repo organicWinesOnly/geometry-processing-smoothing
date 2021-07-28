306fef4d253118cd6631677353ff400281244f32
rhamel.mw <rhamel.rdelpeache@gmail.com>
2021-07-28 14:11:57 -0400

    Complete massmatrix and smooth functions

--- src/massmatrix.cpp
+++ src/massmatrix.cpp
@@ -1,10 +1,27 @@
 #include "massmatrix.h"
+#include "igl/doublearea.h"
+#include <map>
 
+typedef Eigen::Triplet<double> T;
+typedef std::map<int, double> Map;
 void massmatrix(
   const Eigen::MatrixXd & l,
   const Eigen::MatrixXi & F,
   Eigen::DiagonalMatrix<double,Eigen::Dynamic> & M)
 {
-  // Add your code here
+  Eigen::VectorXd Area;
+  igl::doublearea(l, Area);
+  Area = Area / 2.0;
+
+  int number_of_vertices = F.maxCoeff() + 1;
+  M.resize(number_of_vertices);
+
+  for (int i = 0; i < F.rows(); i++)
+  {
+    for (int j = 0; j < 3; j++)
+    {
+      M.diagonal()[F(i, j)] += 1.0 / 3.0 * Area(i);
+    }
+  }
 }
 
--- src/smooth.cpp
+++ src/smooth.cpp
@@ -1,4 +1,9 @@
 #include "smooth.h"
+#include <iostream>
+#include <Eigen/SparseCholesky>
+#include "igl/edge_lengths.h"
+#include "cotmatrix.h"
+#include "massmatrix.h"
 
 void smooth(
     const Eigen::MatrixXd & V,
@@ -8,5 +13,19 @@ void smooth(
     Eigen::MatrixXd & U)
 {
   // Replace with your code
-  U = G;
+  using Sparse = Eigen::SparseMatrix<double>;
+  Eigen::MatrixXd l;
+  igl::edge_lengths(V, F, l);
+
+  Sparse L;
+  cotmatrix(l, F, L);
+  Eigen::DiagonalMatrix<double, Eigen::Dynamic> M;
+  massmatrix(l, F, M);
+
+  Sparse A;
+  Sparse M_sparse(L.rows(), L.cols());
+
+  A = Sparse(M) - lambda * L;
+  Eigen::SimplicialLDLT<Sparse> solver(A);
+  U = solver.solve(M * G);
 }
